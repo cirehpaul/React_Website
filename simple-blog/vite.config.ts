18c664f0821bd010f29a90a80f30a9dda3f52cb8
cirehpaul <cirepaulcruz21@gmail.com>
2026-01-12 19:40:50 +0800

    update release frontend

--- simple-blog/vite.config.ts
+++ simple-blog/vite.config.ts
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-project/', // The name of your repository
   plugins: [react()],
 })
